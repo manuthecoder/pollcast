963bc285a4bdacfcced1d71cb943427521f9686d
manuthecoder <manusvathgurudath@gmail.com>
2022-08-26 08:32:23 +0000

    update _app.tsx and tsconfig.json

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { SessionProvider } from "next-auth/react";
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}) {
+}: any) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />

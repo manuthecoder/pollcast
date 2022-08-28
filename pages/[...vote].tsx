e97ac6d77115c2e19645a8e5b39120f1ceffeec9
manuthecoder <manusvathgurudath@gmail.com>
2022-08-28 06:29:33 +0000

    update [...vote].tsx

--- pages/[...vote].tsx
+++ pages/[...vote].tsx
@@ -1,10 +1,18 @@
 import { Layout } from "../components/Layout";
-import useSWR from "swr"
+import useSWR from "swr";
 
 export default function Vote() {
-  const url = "/api/fetchPoll?id="+ new URLSearchParams({
-    id: window.location.href.split("/vote/")[1]
-  })
-  const {error, data} = useSWR(url, () => fetch(url).then(res => res.json()))
-  return <Layout poll></Layout>;
+  const url =
+    "/api/fetchPoll?id=" +
+    new URLSearchParams({
+      id: window.location.href.split("/vote/")[1],
+    });
+  const { error, data } = useSWR(url, () =>
+    fetch(url).then((res) => res.json())
+  );
+  return <Layout poll>
+    {error && <>An error occured while trying to fetch the poll</>}
+{data ? JSON.stringify(data): <>Loading...</>}
+
+  </Layout>;
 }

6ca531042d813c1a1680319ceb1ea2cfcbb56944
manuthecoder <manusvathgurudath@gmail.com>
2022-08-29 06:22:40 +0000

    update 5 files

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -2,6 +2,6 @@ import { getFeed } from "../../prisma/user";
 
 const handler = async (req: any, res: any) => {
   const feed = await getFeed(req.query.userId ? req.query.userId : undefined);
-  res.status(200).json(feed);
+  res.status(200).json(feed.reverse());
 };
 export default handler;

5fcca079740c1dec2a82637e32c5a127b86836c5
manuthecoder <manusvathgurudath@gmail.com>
2022-08-29 07:02:00 +0000

    update 5 files

--- components/CreatePollDialog.tsx
+++ components/CreatePollDialog.tsx
@@ -210,6 +210,7 @@ export function CreatePollDialog() {
                         background: "#202020",
                       },
                     }}
+                    disabled={loading}
                     variant="contained"
                     disableElevation
                   >
